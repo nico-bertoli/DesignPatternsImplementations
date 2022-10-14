4fd52d5dd5b20a23a3fb0be7d2af8273f1aad33d
nico-bertoli <nicobertoli.1999@gmail.com>
2022-10-14 11:38:02 +0200

    prototype completed

--- Assets/Scenes/Prototype/ClonableChicken.cs
+++ Assets/Scenes/Prototype/ClonableChicken.cs
@@ -1,33 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Playables;
 using UnityEngine;
 
 class ClonableChicken : MonoBehaviour, IPrototype {
 
-    public int value { get; private set;}
+    private static bool isFirstChicken = true;
+    private Value value;
 
-    private void Start() {
-        Debug.Log("created chicken with value: " + value);
+    private void Awake() {
+        value = new Value();
+
+        if (isFirstChicken) {
+            Debug.Log("prototype initialized with value: " + value.MyVal);
+            isFirstChicken = false;
+        } 
     }
 
     public IPrototype Clone() {
         ClonableChicken ris = Instantiate(this,new Vector3(transform.position.x+3,transform.position.y,transform.position.z),Quaternion.identity);
 
-        // doesn't make a lot of sense cause Instantiate already clones the object
-        // for more advanced things this could be more flexible anyway
-        // for example if the chicken had another object associated (and not in his hierarchy, it could be cloned manually here
-        ris.value = this.value;
+        ris.value = new Value(value.MyVal);
 
         ris.GetComponent<Rigidbody>().useGravity = true;
         MyUtils.SetRandomSpeed(ris.gameObject, 30);
-
-        return (IPrototype)ris;
+        return ris;
     }
     /// <summary>
     /// called by button
     /// </summary>
     public void CloneVoid() {
         Clone();
+    }
+
+    public void IncreaseVal() {
+        value.increaseVal();
+    }
+}
+
+class Value {
+    public int MyVal { get; private set; }
+
+    public Value() {
+        MyVal = 0;
+    }
+    public Value(int _val) {
+        MyVal = _val; Debug.Log("clone created with val:" + MyVal);
+    }
+
+    public void increaseVal() {
+        MyVal++;
+        Debug.Log("prototype val increased: " + MyVal);
     }
 }
